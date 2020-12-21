e4eed7d6c148adef1d8e9bae6df0d04f02a9d319
workonfire <kolucki62@gmail.com>
2020-12-21 22:41:42 +0100

    Added debug info

--- src/main/java/pl/workonfire/inventorylogs/util/CommandProcessor.java
+++ src/main/java/pl/workonfire/inventorylogs/util/CommandProcessor.java
@@ -3,13 +3,16 @@ package pl.workonfire.inventorylogs.util;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class CommandProcessor {
+public abstract class CommandProcessor {
 
     public String getName(Object commandClass) {
         Class<?> clazz = commandClass.getClass();
         if (clazz.isAnnotationPresent(Command.class))
             return clazz.getAnnotation(Command.class).name();
-        return null;
+        else {
+            System.err.println("WARNING: The class " + clazz + " does not have a proper @Command annotation.");
+            return null;
+        }
     }
 
     public void register(JavaPlugin pluginInstance, CommandExecutor commandClass) {
